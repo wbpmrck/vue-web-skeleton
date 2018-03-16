f7b1c2f45044289dde5604cd22d0bfab84dae989
kaicui <wbpmrck2003@gmail.com>
2018-03-16 16:25:47 +0800

    add precss config

--- front-end/src/view/login/login.vue
+++ front-end/src/view/login/login.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div id="login">
+
+    <input type="text" class="form-input" id="phone" placeholder="请输入手机号或邮箱"/>
+    <input type="text" class="form-input" id="password" placeholder="请输入密码"/>
+
+    <div id="operation">
+      <div id="regist">注册</div>
+      <div id="forget-password">忘记密码</div>
+    </div>
+
   </div>
 </template>
 
@@ -8,40 +16,63 @@
 
   import userService from  "@/service/user-service";
 
-export default {
-  name: 'login',
-  data () {
-    return {
-      msg: '这是一个登录页面'
-    }
-  },
-  created(){
-    userService.login({accountName:"accountName"}).then(function (resp) {
-      if(resp && resp.success){
-
-      }else{
-
+  export default {
+    name: 'login',
+    data () {
+      return {
+        msg: '这是一个登录页面'
       }
-    }).catch(function(err){
-    });
+    },
+    created(){
+      userService.login({accountName:"accountName"}).then(function (resp) {
+        if(resp && resp.success){
+
+        }else{
+
+        }
+      }).catch(function(err){
+      });
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<!--<style scoped>-->
+<style>
+  #login{
+    margin-top: 24px;
+
+  #phone,#password{
+    width: 200px;
+    margin: 0 20px;
+
+    color:#6B6B6B;
+    background-color:#000;
+
+    font-size: 20px;
+    height:52px;
+    line-height: 20px;
+    font-weight: 500;
+
+    border: none;
+    border-bottom: solid 1px #6B6B6B;
+  }
+
+  #operation {
+    height: 16px;
+    margin-top: 17px;
+    padding: 0 20px;
+    font-size: 16px;
+  }
+
+  #regist {
+    display: inline-block;
+    float: left;
+  }
+  #forget-password {
+    display: inline-block;
+    float: right;
+  }
+  }
+
 </style>
